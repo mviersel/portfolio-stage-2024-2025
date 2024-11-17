e895714c8faffbb55d62fd339ce2b92aec7f1ed3
mviersel <119597405+mviersel@users.noreply.github.com>
2024-11-17 13:10:58 +0100

    Nieuwe opbouw voor netlify

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <header>
+            Dit is een header.
+        </header>
+
+        <div>
+            <slot />
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/projecten.vue
@@ -0,0 +1,13 @@
+<template>
+
+<MainHeader />
+
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
