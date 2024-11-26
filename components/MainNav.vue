f49b5ae2e3427922523a9c39eab5152d115e1755
mviersel <119597405+mviersel@users.noreply.github.com>
2024-11-26 19:01:37 +0100

    Push

---
+++ components/MainNav.vue
@@ -0,0 +1,15 @@
+<template class="d:px-20 lg:px-40">
+    <nav class="py-10 mb-12 flex justify-between text-gray-600">
+        <NuxtLink to="/"><img src="/assets/img/Logo-gray-transparent.png" class="h-10"></nuxtlink>
+        <ul class="flex items-center">
+            <li><NuxtLink to="/" class="hover:text-gray-400 mr-5">Home</NuxtLink></li>
+            <li><NuxtLink to="" class="hover:text-gray-400">Home</NuxtLink></li>
+        </ul>
+    </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
--- pages/projecten/speeltuin.vue
+++ pages/projecten/speeltuin.vue
@@ -1,13 +1,19 @@
-<template>
+<template >
+    
+<section class="md:px-20 lg:px-40">
+    <MainNav />
+
+    
     <div>
-Speeltuin
+
     </div>
+    Speeltuin
+</section>
+
 </template>
 
 <script setup>
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
