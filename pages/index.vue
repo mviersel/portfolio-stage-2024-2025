ac2e46024057e148fb4d2278b91e98980dea7e60
mviersel <119597405+mviersel@users.noreply.github.com>
2025-01-27 16:14:27 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -13,8 +13,8 @@
 
                 <div class="text-center">
                     <div class="flex justify-center mb-16">
-                        <img src="https://media.licdn.com/dms/image/v2/D4E03AQEQTt422QJhhA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1681157489564?e=1743638400&v=beta&t=wfWLHNWD4EEw4jrKmHlUrYH_USxARmP5lXpVZ_BWtKk"
-                            alt="Image" class="grayscale hover:grayscale-0 rounded-full">
+                        <img src="https://media.licdn.com/dms/image/v2/D4E03AQEQTt422QJhhA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1681157489564?e=1743638400&v=beta&t=wfWLHNWD4EEw4jrKmHlUrYH_USxARmP5lXpVZ_BWtKk "
+                            alt="Image" class="grayscale hover:grayscale-0 rounded-full h-80">
                     </div>
                     <h6 class="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-8">Martijn van Iersel</h6>
                     <h1 class="font-normal text-gray-900 text-4xl md:text-7xl leading-none mb-8">ICT & Media student
